dc5fae8e4bf30c479f50fae5e68bfeaa7440052c
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-22 15:28:37 +0800

    using if statement

--- exercise 9.py
+++ exercise 9.py
@@ -1,3 +1,8 @@
 # let's make a def function
 def palindrome(number):
     reverse = int(str(number)[::-1]) # make this to check if the reverse of char is stil the same
+    #if statement
+    if number == reverse:
+        print(f"original number {number}")
+        print("Yes. given number is palindrome number")
+        return True
